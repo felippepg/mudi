70683bf99b0c71d39b2943ab3856f033b47460b3
Felippe Pires <fpires.fga@gmail.com>
2021-04-16 06:41:08 -0300

    Primeiro commit

---
+++ src/test/java/br/com/piresfoundation/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piresfoundation.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
