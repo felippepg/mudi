2f7f7e1bf007013ab0f45c2e62f1ec4aa0938071
Felippe Pires <fpires.fga@gmail.com>
2021-04-18 22:52:29 -0300

    Formulario de cadastro parcialmente pronto com validações de campo

---
+++ src/main/java/br/com/piresfoundation/mudi/controller/PedidoController.java
@@ -0,0 +1,39 @@
+package br.com.piresfoundation.mudi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.piresfoundation.mudi.dto.RequisicaoNovoPedido;
+import br.com.piresfoundation.mudi.models.Pedido;
+import br.com.piresfoundation.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+	@Autowired
+	PedidoRepository repository;
+
+	@GetMapping("formulario")
+	public String formulario(RequisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+
+    @PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		if (result.hasErrors()) {
+			return "pedido/formulario";
+		}
+
+		Pedido pedido = requisicao.toPedido();
+		repository.save(pedido);
+		return "home";
+	}
+}
